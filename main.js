a22edc2e2c2eebf182ae9e264194ff87cfadda1a
Mason Austin <mason47austin@gmail.com>
2023-04-25 21:43:36 -0500

    created all event listners

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
 const data = [
     {
-      videoId: 'cNjIUSDnb9k',
-      title: 'Callbacks',
-      category: 'javascript',
-      favorite: true,
-    },
-    {
-      videoId: 'f02mOEt11OQ',
-      title: 'Chill Beats',
-      category: 'music',
-      favorite: false,
-    },
-    {
-      videoId: 'PlxWf493en4',
-      title: 'How to Make a Super Simple Website',
-      category: 'html',
-      favorite: false,
-    },
-    {
-      videoId: '1PnVor36_40',
-      title: 'Learn CSS in 20 Minutes',
-      category: 'css',
-      favorite: true,
-    },
-    {
       videoId: '1Rs2ND1ryYc',
       title: 'Zero to Hero',
-      category: 'css',
+      category: 'coding',
       favorite: false,
     },
     {
       videoId: 'jjydMpW47wk',
       title: 'Inspo on JS',
-      category: 'javascript',
+      category: 'coding',
       favorite: true,
     },
 ];
@@ -57,33 +33,33 @@ const addVideoModel = () =>{
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
+          <form id="addForm">
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="youtubeId" placeholder="YouTube Video ID" required>
+              <label for="floatingInput">YouTube Video ID</label>
+            </div>
 
-          <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="youtubeId" placeholder="YouTube Video ID" required>
-            <label for="floatingInput">YouTube Video ID</label>
-          </div>
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="title" placeholder="Title" required>
+              <label for="floatingInput">Title</label>
+            </div>
 
-          <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="title" placeholder="Title" required>
-            <label for="floatingInput">Title</label>
-          </div>
+            <select id="categorySelector" class="form-select" aria-label="Default select example">
+              <option selected>Open this select menu</option>
+              <option value="Critical Role">Critical Role</option>
+              <option value="Coding">Coding</option>
+              <option value="Cars">Cars</option>
+            </select>
 
-          <select class="form-select" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">Critical Role</option>
-            <option value="2">Coding</option>
-            <option value="3">Cars</option>
-          </select>
-
-          <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-            <label class="form-check-label" for="inlineCheckbox1">Favorite</label>
-          </div>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="">
+              <label class="form-check-label" for="inlineCheckbox1">Favorite</label>
+            </div>
+            <footer></footer>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </footer>
+          </form>
         </div>
       </div>
     </div>
@@ -91,35 +67,36 @@ const addVideoModel = () =>{
   renderToDom("addVideoContainer", domString)
 }
 
- const vidPlayer= () =>{
+ const vidPlayer= (videoID) =>{
+  const video=videoID
   let domstring=`<iframe width="420" height="315"
-    src="https://www.youtube.com/embed/tgbNymZ7vqY">
+    src="https://www.youtube.com/embed/${video}">
   </iframe>`
   renderToDom("videoPlayer", domstring)
  }
 
 const filterBtns = () => {
   const domString = `
-  <button type="button" class="btn btn-primary">Criticle Role</button>
-  <button type="button" class="btn btn-primary">Codeing</button>
-  <button type="button" class="btn btn-primary">Cars</button>
-  <button type="button" class="btn btn-primary">Favorites</button>
-  <button type="button" class="btn btn-primary">Clear Filters</button>`
+  <button type="button" class="btn btn-primary" id="roleBtn">Criticle Role</button>
+  <button type="button" class="btn btn-primary" id="codingBtn">Coding</button>
+  <button type="button" class="btn btn-primary" id="carsbtn">Cars</button>
+  <button type="button" class="btn btn-primary" id="favBtn">Favorites</button>
+  <button type="button" class="btn btn-primary" id="clearBtn">Clear Filters</button>`
   renderToDom("filterContainer", domString)
 }
 
-const videoCards= () =>{
+const videoCards= (array) =>{
   let domString=``
-  data.forEach(video => {
+  array.forEach(video => {
 
     domString+=`
-    <div class="card">
+    <div class="card ${video.favorite}">
       <div class="card-body">
         <img src="https://i.ytimg.com/vi/HFTvaOjWk2c/maxresdefault.jpg" width="200px">
         <div>
           <h5>${video.title}</h5>
           <h6>Category:${video.category}</h6>
-          <button>Watch Video</button>
+          <button id="watchBtn">Watch Video</button>
         </div>
         <button>Delete</button>
       </div>
@@ -135,11 +112,67 @@ const videoCards= () =>{
 
 }
 
+const filter = (array, typeString) => {
+  const typeArray=[]
+  for (const video of array){
+    if (video.category===typeString)
+    typeArray.push(video)
+
+  }
+  return typeArray
+}
+
 let startApp = () =>{
   addVideoModel();
   filterBtns();
-  videoCards();
-  vidPlayer();
+  videoCards(data);
+  vidPlayer(data[0].videoId);
 }
-
 startApp()
+
+const critRoleBtn = document.getElementById("roleBtn")
+const codeBtn = document.getElementById("codingBtn")
+const carBtn = document.getElementById("carsbtn")
+const favBtn = document.getElementById("favBtn")
+const clearBtn = document.getElementById("clearBtn")
+const mainForm = document.getElementById("addForm")
+
+critRoleBtn.addEventListener("click", () =>{
+  const critRoleVideos=filter(data,"Critical Role")
+  videoCards(critRoleVideos)
+} )
+
+codeBtn.addEventListener("click", () => {
+  const codingVideos=filter(data,"coding")
+  videoCards(codingVideos)
+})
+
+carBtn.addEventListener("click", () => {
+  const carVideos=filter(data,"car")
+  videoCards(carVideos)
+})
+
+clearBtn.addEventListener("click", () => {
+  videoCards(data)
+})
+
+favBtn.addEventListener("click", () => {
+  const favArry=[]
+  for (const video of data) {
+    if (video.favorite===true) {
+      favArry.push(video)
+    }
+  }
+  videoCards(favArry)
+})
+
+mainForm.addEventListener("submit", () => {
+  console.log("functinwork")
+  data.push({
+    videoId : document.getElementById("youtubeId").value,
+    title: document.getElementById("title").value,
+    category: document.getElementById("categorySelector").value,
+    favorite:d
+  })
+  videoCards(data)
+})
